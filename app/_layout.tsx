d37ad637bb508c59ca97091968c96245cf61b1de
Stuart Muyambi <stuartmuyambi@gmail.com>
2025-05-30 23:11:19 +0300

    Refactor RootLayout component for consistent formatting and improved readability

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="about" options={{ title: 'About' }} />
+      <Stack.Screen name="index" options={{ title: "Home", headerLeft: () => <></> }} />
+      <Stack.Screen name="about" options={{ title: "About" }} />
       <Stack.Screen name="+not-found" options={{ headerShown: false }} />
-    </Stack> 
+    </Stack>
   );
 }
