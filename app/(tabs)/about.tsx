7911b5201225a5cc67dbce1f3a5c714aec29b430
Stuart Muyambi <stuartmuyambi@gmail.com>
2025-05-31 00:15:52 +0300

    Implement tab navigation and restructure screens for improved organization

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello,  am the About screen</Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Home", headerLeft: () => <></> }} />
-      <Stack.Screen name="about" options={{ title: "About" }} />
-      <Stack.Screen name="+not-found" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" options={{ title: 'Oops! Not Found' }} />
+      </Stack>
+    </>
   );
 }
