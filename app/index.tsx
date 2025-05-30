d20807de1c149b13068bbe980a1f04584aa23583
Stuart Muyambi <stuartmuyambi@gmail.com>
2025-05-30 19:39:10 +0300

    Refactor Index component to use StyleSheet for styling and improve code readability

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>Hello World!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 24,
+  },
+});
